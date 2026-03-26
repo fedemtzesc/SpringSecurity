853dee2c68ca7360e17175c444899b98fe4f1c8d
Federico Martinez <fedemtzesc@hotmail.com>
2026-03-26 12:45:00 -0600

    VERSION #5 - Se agrega un metodo manejador para cuando la autenticacion es exitosa, para redirigirnos a una pagina en especifico, desde el formLogin

--- src/main/java/com/fdxsoft/SpringSecurity/SecurityConfig.java
+++ src/main/java/com/fdxsoft/SpringSecurity/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 @Configuration
 @EnableWebSecurity
@@ -19,8 +20,17 @@ public class SecurityConfig {
                     auth.requestMatchers("/v1/index2").permitAll();
                     auth.anyRequest().authenticated();
                 })
-                .formLogin(Customizer.withDefaults())
+                .formLogin(form -> form
+                        .successHandler(this.successHandler()) // Si las credenciales son correctas, se ejecuta el
+                                                               // successHandler
+                        .permitAll())
                 .build();
+    }
+
+    public AuthenticationSuccessHandler successHandler() {
+        return (request, response, authentication) -> {
+            response.sendRedirect("/v1/index");
+        };
     }
 
 }
