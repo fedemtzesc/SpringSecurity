3ac1aa0e535a32a0ec50eee266e8ff49f51f74fc
Federico Martinez <fedemtzesc@hotmail.com>
2026-03-26 11:42:08 -0600

    Se agrego explicaicion para saber como hacer el log out en este modo mas basico de Spring Security

--- src/main/java/com/fdxsoft/SpringSecurity/controller/CustomerController.java
+++ src/main/java/com/fdxsoft/SpringSecurity/controller/CustomerController.java
@@ -1,5 +1,12 @@
 package com.fdxsoft.SpringSecurity.controller;
 
+/**
+ * Cuando queramos salir del modo de seguridad por defecto, debemos teclear en
+ * el navegador la siguiente url: http://localhost:8080/logout
+ * el sistema nos presentara un mensaje para que confirmemos, le hacemos click
+ * en el boton de Log Out y el sistema nos sacara.
+ */
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
